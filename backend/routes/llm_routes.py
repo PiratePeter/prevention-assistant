1a15d57ae8b69375483c3b7afd616a35077ec2ca
Alexander Peter <alexpeter95@hotmail.com>
2025-08-24 00:31:31 +0200

    implement POST regenpreventions endpoint

--- backend/routes/llm_routes.py
+++ backend/routes/llm_routes.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, request
 from services.llm_service import get_llm_answer_logic
 
-llm_bp = Blueprint('llm_routes', __name__, url_prefix='/api/llm')
+llm_bp = Blueprint('llm_routes', __name__, url_prefix='/api/regenpreventions')
 
-@llm_bp.route("", methods=["GET"])
+@llm_bp.route("", methods=["POST"])
 def get_llm_answer():
     return get_llm_answer_logic(request)
 
--- backend/services/llm_service.py
+++ backend/services/llm_service.py
@@ -3,11 +3,14 @@ from google import genai
 
 client = genai.Client()
 
-def get_llm_answer_logic(_):
-    prompt = 'Explain how AI works in a few words'
+def get_llm_answer_logic(request):
+    data = request.json
+    prompt = data.get('prompt')
+    preventions = data.get('preventions')
+
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents=prompt
+        model='gemini-2.5-flash', contents=prompt+"\n\n"+preventions
     )
 
-    dummy_data = {'prompt': prompt, 'response': response.text}
-    return jsonify(dummy_data), 200
+    return_data = {'preventions': response.text}
+    return jsonify(return_data), 200
